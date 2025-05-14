3c8a826ec9116def16be2d784ac07ad5fb9f1dab
Renato Barros <rennatobarros@gmail.com>
2025-05-14 01:27:54 -0300

    update eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
       <Button>Click me</Button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
